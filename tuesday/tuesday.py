17810d6f6a0b2eb05905b8095ee94d7981fc2f79
Monosij Bhowmik <33190748+monosij-bhowmik@users.noreply.github.com>
2018-11-13 00:17:16 +0530

    Add files via upload

---
+++ tuesday/tuesday.py
@@ -0,0 +1,29 @@
+#pyaudio and SpeechRecognition required.
+#GTTS Google Text To Speech library required for speaking out.
+
+
+import speech_recognition as sr
+import gTTS as gt
+
+#record audio
+
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    r.adjust_for_ambient_noise(source, duration=1)
+    print("Please enter the password...")
+    audio = r.listen(source, phrase_time_limit=3)
+
+#Recognize Speech and text output
+
+try:
+    value = r.recognize_google(audio)
+    r.recognize_google(audio)
+    print("You Said :"+ r.recognize_google(audio))
+    if value == 'hey Tuesday':
+        print("Hey Rohit, I'm Tuesday how can I help you ?")
+    else:
+        print("Incorrect Pasword")
+except sr.UnknownValueError:
+    print("Google Speech Recognition couldn't understand")
+except sr.RequestError as e:
+    print("Could not request results".format(e))
